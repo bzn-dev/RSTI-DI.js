fa6fbf935d14f0eeb375467ced0f01670ad1d462
bznfsj <rafaelbauer0406@gmail.com>
2024-02-16 16:46:31 -0300

    all updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // this is a js file
 // Rafael Bauer
 
-// Retorna o nome digitado pelo usuário e a cor favorita digitada pelo usuário, 
+// Retorna o nome digitado pelo usuário e a cor favorita digitada pelo usuário,
 // sem nenhum tipo de filtração ou correção.
 
 /* var nome = prompt("Qual é o seu nome?");
@@ -60,10 +60,56 @@ console.log(racasCachorro[option]); */
 /* const pokemon = ["bullbasauro", "squirtle", "charmander"];
 console.log(pokemon.length); // .lenght = quanto tem, nesse caso, a quantia de itens na array */
 
-// Splice 
+// Splice
 
-var array = ["a", "b", "c", "d", "e", "f", "g", "h"]
+/* var array = ["a", "b", "c", "d", "e", "f", "g", "h"]
 //           [0    1    2    3    4    5    6    7 ]
 
 array.splice(3, 1) // .splice(aPartirDe, quantiaDeRemoções)
 console.log(array)
+ */
+
+// Sequência de utilizações dos modificadores na array
+
+/* var array = ["1", "2", "3", "4", "5", "6"];
+
+console.log(array.length);
+console.log("adicionando o número 7...");
+array.push("7");
+console.log(array);
+console.log("removendo o número 4 e 5...")
+array.splice(4, 2);
+console.log(array);
+console.log(array.length); */
+
+// Forma demorada de calcular a área de um retângulo
+
+/* const altura = 2
+const largura = 3
+const area = altura * largura
+console.log(area); */
+
+// Forma mais rápida e usando uma function para fazer vários cálculos de área
+
+/* function calcularArea(altura, largura) {
+    const area = altura * largura;
+    console.log(area);
+}
+
+calcularArea(2, 3.5); */
+
+// Função para imprimir "Olá Mundo"
+
+/* function imprimirOlaMundo() {
+    console.log("Olá Mundo")
+}
+
+imprimirOlaMundo() */
+
+// Função para facilitar a impressão de um nome no console
+
+/*  function receberNome(nome) {
+    console.log(`Olá ${nome}`)
+}
+
+receberNome("Rafael")  */
